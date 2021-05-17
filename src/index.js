1c02846fdfd94dcb707f1cfb402d47d8a5f8b1ef
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-05-17 15:17:31 +0700

    [kyomel] find all and find by id using mongoose

--- src/index.js
+++ src/index.js
@@ -15,7 +15,28 @@ app.post('/users', (req, res) => {
   }).catch((err) => {
     res.status(400).send(err);
   })
-});
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch((err) => {
+        res.status(500).send();
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }).catch((err) => {
+        res.status(500).send();
+    })
+})
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
@@ -24,6 +45,27 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task);
     }).catch((err) => {
         res.status(400).send(err);
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((err) => {
+        res.status(500).send();
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return res.status(404).send();
+        }
+        res.send(task)
+    }).catch((err) => {
+        res.status(500).send();
     })
 })
 
