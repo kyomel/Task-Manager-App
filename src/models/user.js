5a7776d5bc6703252a9a7a093fe78024019a8d8e
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-05-20 16:22:41 +0700

    [kyomel] delete cascade

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -52,7 +53,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.virtual('tasks', {
     ref: 'Task',
-    localField: '_id',
+    localField: '_id ',
     foreignField: 'owner'
 })
 
@@ -96,6 +97,14 @@ userSchema.pre('save', async function (next) {
     }
     next();
 })
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id });
+    next();
+})
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
