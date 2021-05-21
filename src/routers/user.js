91e712c5446b9557fa1600d575c0b054eba309e9
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-05-21 17:02:24 +0700

    [kyomel] add feature multer to upload image at user router

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const router = new express.Router();
 const auth = require('../middleware/auth');
 const User = require('../models/user');
@@ -75,6 +76,23 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (err) {
         res.status(500).send();
     }
+})
+
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg||jpeg||png)$/)) {
+            return cb(new Error('Please upload an image!'))
+        }
+        cb(undefined, true);
+    }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
 })
 
 module.exports = router;
