854c2dbbfeaf1f204f7650942874214d2bc48c0b
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-05-29 18:25:40 +0700

    [kyomel] refactoring index.js

--- src/math.js
+++ src/math.js
@@ -7,8 +7,21 @@ const fahrenheitToCelsius = (temp) => {
 const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32;
 }
+
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Numbers must be non-negative')
+            }
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
