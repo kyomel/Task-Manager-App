2fd1c97054f91c615b3a3757c74b9306917d43ab
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-05-30 21:21:20 +0700

    [kyomel] user testing suites

---
+++ test/__mocks__/mailgun/mail.js
@@ -0,0 +1,15 @@
+module.exports = function(apiKey, domain){
+    const object2 = {
+        send() {
+     
+        }
+      }
+     
+      const object1 = {
+        messages() {
+          return object2
+        }
+      }
+     
+      return object1
+}
